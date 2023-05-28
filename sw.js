184bbb17013b70380b836683be169440d8a22a2c
tobyyosoba777 <tobiosoba777@gmail.com>
2023-05-28 18:26:39 -0500

    updated

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./style.css", "./icon/android-chrome-192x192.png"]);
+            return cache.addAll(["./", "./style.css", "./icon/android-chrome-192x192.png"]);
         })
     );
 })
 
 self.addEventListener("fetch", e => {
-    console.log(`Intercepting fetch request for ${e.request.url}`);
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    )
 });
