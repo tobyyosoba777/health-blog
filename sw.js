bfef530e9c5fa41caebfcce196ad566c89dd0025
tobyyosoba777 <tobiosoba777@gmail.com>
2023-05-28 17:46:43 -0500

    updated

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", e => {
-    console.log("Install towbbie!");
+    e.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./style.css", "./icon/android-chrome-192x192.png"]);
+        })
+    );
 })
